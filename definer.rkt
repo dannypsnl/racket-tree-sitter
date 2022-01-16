d42dd9db52b4486de84cbc5a80f123102f5fd29a
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2022-01-16 20:44:59 +0800

    api
    - parser delete
    - get language
    - set language

--- definer.rkt
+++ definer.rkt
@@ -6,6 +6,13 @@
   (ffi-lib "./zig-out/lib/libtree-sitter" '(#f)))
 
 (define _TSParserRef (_cpointer 'TSParser))
+(define _TSLanguageRef (_cpointer 'TSLanguage))
 
 (define-treesitter parser-new (_fun -> _TSParserRef)
   #:c-id ts_parser_new)
+(define-treesitter parser-delete (_fun _TSParserRef -> _void)
+  #:c-id ts_parser_delete)
+(define-treesitter set-language (_fun _TSParserRef _TSLanguageRef -> _bool)
+  #:c-id ts_parser_set_language)
+(define-treesitter get-language (_fun _TSParserRef -> _TSLanguageRef)
+  #:c-id ts_parser_language)
