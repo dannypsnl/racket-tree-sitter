2a04c6bc9687004f020d5912b8d2d1ec3993abe8
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2022-01-19 13:05:57 +0800

    installation

--- build.zig
+++ build.zig
@@ -2,9 +2,6 @@ const std = @import("std");
 const Builder = std.build.Builder;
 
 pub fn build(b: *Builder) !void {
-    var general = std.heap.GeneralPurposeAllocator(.{}){};
-    defer _ = general.deinit();
-
     const mode = b.standardReleaseOptions();
     const lib = b.addSharedLibrary("tree-sitter", null, .unversioned);
     const flags = [_][]const u8{
@@ -16,6 +13,5 @@ pub fn build(b: *Builder) !void {
     lib.addCSourceFile("./tree-sitter/lib/src/lib.c", &flags);
     lib.setBuildMode(mode);
 
-    b.default_step.dependOn(&lib.step);
-    b.installArtifact(lib);
+    lib.install();
 }
