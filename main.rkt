6a682bf718cd449ad17f7fd3ac41db43fcca681f
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2022-01-20 03:56:04 +0800

    use `allocator` to manage parser in GC

--- main.rkt
+++ main.rkt
@@ -1,11 +1,14 @@
 #lang racket/base
 (require ffi/unsafe
+         ffi/unsafe/alloc
          "definer.rkt")
 
 (define-treesitter commonlisp (_fun -> _TSLanguageRef)
   #:c-id tree_sitter_commonlisp)
 
-(define p (parser-new))
+(define make-parser ((allocator parser-delete) parser-new))
+
+(define p (make-parser))
 (set-language p (commonlisp))
 
 (define source-code "(+ 1 2)")
@@ -15,4 +18,3 @@
 (displayln (node->string root))
 
 (tree-delete tree)
-(parser-delete p)
