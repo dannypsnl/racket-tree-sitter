2455ddbddf5011736dc466feb2594d02b31c5c44
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2022-08-17 18:47:25 +0800

    introduce parameter for customize search directories
    Signed-off-by: Lîm Tsú-thuàn <dannypsnl@gmail.com>

--- definer.rkt
+++ definer.rkt
@@ -1,10 +1,16 @@
 #lang racket/base
-(provide define-treesitter)
+(provide define-treesitter
+         custom-search-dirs)
 (require ffi/unsafe
-         ffi/unsafe/define)
+         ffi/unsafe/define
+         setup/dirs)
+
+(define custom-search-dirs (make-parameter '()))
 
 (define (get-lib-dir)
-  (list "/usr/local/lib"))
+  (append '("/usr/lib" "/usr/local/lib")
+          (custom-search-dirs)
+          (get-lib-search-dirs)))
 (define-ffi-definer define-treesitter
   (ffi-lib "libtree-sitter" '(#f)
            #:get-lib-dirs get-lib-dir))
